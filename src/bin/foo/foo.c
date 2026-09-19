c73f5236828424323f73b3bd66c3f1eb1823a51b
miranda-mir <mir383@proton.me>
2026-09-19 15:08:58 -0300

    adicionar arquivos basicos
    adicionado: arquivos basicos, templates, exemplos, diretorios e organizaçao basica

---
+++ src/bin/foo/foo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+    Exemplo de como organizar programas
+    em diretorios como "bin/", "sbin/",
+    "usr.bin" e "usr.sbin".
+
+    Arquivos dentro de foo/ como foo.c, Makefile e
+    foo.1 sao todos de dominio publico.
+*/
+
+main()
+{
+    printf("bar!\n");
+    return 0;
+}
