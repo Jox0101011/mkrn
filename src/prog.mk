9a3b9fa6bf09071c609644808445bce1c11274a0
miranda-mir <mir383@proton.me>
2026-09-24 14:10:56 -0300

    melhorar prog.mk

--- src/prog.mk
+++ src/prog.mk
@@ -1,41 +1,95 @@
 # prog.mk
-# 
-# [USAGE]-~~--~~--~~~--~~~~--~~~-~~~--|
-# |make -f prog.mk          |
-# |make -f prog.mk all      |
-# |make -f prog.mk clean    |
-# |make -f prog.mk install  | 
-# |make -f prog.mk .c.o     |
-# |___________________________________|
-#
-# *-----------------------------*
-# .PHONY -> all clean install
-# *-----------------------------*
 
-CC ?=            cc
-CFLAGS ?=        -Os -pipe
-PREFIX ?=        /usr/local
-BINDIR ?=        ${PREFIX}/bin
-
-ifndef SRCS
-OBJS=   ${PROG}.o
-else
-OBJS=   ${SRCS:.c=.o}
+ifndef PROG
+$(error PROG is not set, use e.g. 'make PROG=foo')
 endif
 
-all: ${PROG}
+.DEFAULT_GOAL := all
 
-.c.o:
-	${CC} ${CFLAGS} -c $< -o $@
+CC       ?= cc
+INSTALL  ?= install
+RM       ?= rm -f
+MKDIR_P  ?= mkdir -p
 
-${PROG}: ${OBJS}
-	${CC} ${CFLAGS} ${LDFLAGS} ${OBJS} ${LDADD} -o $@
+PREFIX   ?= /usr/local
+BINDIR   ?= $(PREFIX)/bin
+MANDIR   ?= $(PREFIX)/share/man
+MANSECTION ?= 1
+
+CPPFLAGS ?=
+CFLAGS   ?= -Os -pipe
+LDFLAGS  ?=
+LDLIBS   ?=
+LDADD    ?=
+
+SRCS     ?= $(PROG).c
+
+OBJS     := $(SRCS:.c=.o)
+DEPS     := $(OBJS:.o=.d)
+
+BUILD ?= release
+
+ifeq ($(BUILD),debug)
+CFLAGS += -O0 -g3
+else ifeq ($(BUILD),release)
+CFLAGS += -DNDEBUG
+endif
+
+ifneq ($(strip $(SANITIZE)),)
+CFLAGS  += -fsanitize=$(SANITIZE)
+LDFLAGS += -fsanitize=$(SANITIZE)
+endif
+
+
+.PHONY: all clean install install-bin install-man install-strip \
+        uninstall help
+
+all: $(PROG)
+
+
+%.o: %.c
+	$(CC) $(CPPFLAGS) $(CFLAGS) -MMD -MP -c $< -o $@
+
+
+$(PROG): $(OBJS)
+	$(CC) $(LDFLAGS) $(OBJS) $(LDLIBS) $(LDADD) -o $@
+
+
+-include $(DEPS)
+
 
 clean:
-	rm -f ${PROG} ${OBJS}
+	$(RM) $(PROG) $(OBJS) $(DEPS)
 
-install: all
-	mkdir -p ${DESTDIR}${BINDIR}
-	install -m 755 ${PROG} ${DESTDIR}${BINDIR}/${PROG}
 
-.PHONY: all clean install
+install: install-bin install-man
+
+install-bin: all
+	$(MKDIR_P) "$(DESTDIR)$(BINDIR)"
+	$(INSTALL) -m 755 "$(PROG)" "$(DESTDIR)$(BINDIR)/$(PROG)"
+
+install-strip: all
+	$(MKDIR_P) "$(DESTDIR)$(BINDIR)"
+	$(INSTALL) -s -m 755 "$(PROG)" "$(DESTDIR)$(BINDIR)/$(PROG)"
+
+
+ifneq ($(strip $(MAN)),)
+
+install-man:
+	$(MKDIR_P) "$(DESTDIR)$(MANDIR)/man$(MANSECTION)"
+	$(INSTALL) -m 644 "$(MAN)" \
+		"$(DESTDIR)$(MANDIR)/man$(MANSECTION)/$$(basename $$(notdir $(MAN))).$(MANSECTION)"
+
+uninstall:
+	$(RM) "$(DESTDIR)$(BINDIR)/$(PROG)"
+	$(RM) "$(DESTDIR)$(MANDIR)/man$(MANSECTION)/$$(basename $$(notdir $(MAN))).$(MANSECTION)"
+
+else
+
+install-man:
+	@:
+
+uninstall:
+	$(RM) "$(DESTDIR)$(BINDIR)/$(PROG)"
+
+endif
